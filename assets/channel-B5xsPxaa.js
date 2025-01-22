add6dc241119ffaf941905d97962e9574737bc1c
LincZero <762699299@qq.com>
2025-01-22 04:31:41 +0000

    Deploying to gh-pages from @ LincDocs/MdNote_Public@30554407c47294b227d29d2fd1163a6e18fdd8a2 🚀

---
+++ assets/channel-B5xsPxaa.js
@@ -0,0 +1,1 @@
+import{ah as o,ai as n}from"./mermaid.core-E5hcdpoE.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
---
+++ assets/diy码表.html-Bk7mgHpM.js
@@ -0,0 +1,101 @@
+import{_ as i,c as e,e as d,o as l}from"./app-nyIV-ksP.js";const s={};function a(v,n){return l(),e("div",null,n[0]||(n[0]=[d(`<h1 id="diy码表" tabindex="-1"><a class="header-anchor" href="#diy码表"><span>diy码表</span></a></h1><h1 id="目录" tabindex="-1"><a class="header-anchor" href="#目录"><span>目录</span></a></h1><h1 id="v1" tabindex="-1"><a class="header-anchor" href="#v1"><span>v1</span></a></h1><h2 id="_1码区" tabindex="-1"><a class="header-anchor" href="#_1码区"><span>1码区</span></a></h2><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>&#39; 1码区
+q,1=去
+q,2=却
+q,3=全
+w,1=我
+w,2=为 &#39; 因为以为称为是为不为
+wu,1=无
+ww,1=未
+e,1=二
+e,2=而
+r,1=人
+r,2=入
+bi,1=比
+bi,2=必
+
+d,1=的
+d,2=但
+d,3=打
+yu,2=于是
+&#39; ku,2=可是
+&#39; du,2=但是
+&#39; bu,2=不是
+&#39; ju,2=就是
+j,2=仅
+vu,1=主
+vu,2=只是
+&#39; 这是
+ky,2=可以
+yw,1=因为
+yw,2=以为
+iw,2=成为
+uw,2=是为
+bw,2=不为
+ny,1=那样
+nay,1=哪样
+nav,1=哪种
+nag,1=哪个
+sm,1=什么
+wsm,1=为什么
+usm,1=是什么
+gsm,1=干什么
+&#39; 无理
+kw,2=因为
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h1 id="数出" tabindex="-1"><a class="header-anchor" href="#数出"><span>数出</span></a></h1><h2 id="基本" tabindex="-1"><a class="header-anchor" href="#基本"><span>基本</span></a></h2><p>字词分</p><p>1码：10重</p><p>2码：1出字，2出词，4出成语</p><p>数字区</p><p>1234</p><p>56</p><p>7890</p><h2 id="码区" tabindex="-1"><a class="header-anchor" href="#码区"><span>码区</span></a></h2><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>q,1=求
+q,2=穷
+q,4=全
+q,7=去
+q,8=棋
+
+w
+
+e
+
+r
+
+t
+
+y
+
+u
+
+i
+
+o
+
+p
+
+a
+
+s
+
+d
+
+f
+
+g
+
+h
+
+j
+
+k
+
+l
+
+z
+
+x
+
+c
+
+v
+
+b
+
+n
+
+m
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>qa</p><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h2 id="双拼声韵表" tabindex="-1"><a class="header-anchor" href="#双拼声韵表"><span>双拼声韵表</span></a></h2>`,19)]))}const c=i(s,[["render",a],["__file","diy码表.html.vue"]]),t=JSON.parse(`{"path":"/03.%20SkillAndProduction/%E8%BE%93%E5%85%A5%E6%B3%95/diy%E7%A0%81%E8%A1%A8.html","title":"diy码表","lang":"zh-CN","frontmatter":{"description":"diy码表 目录 v1 1码区 数出 基本 字词分 1码：10重 2码：1出字，2出词，4出成语 数字区 1234 56 7890 码区 qa 双拼声韵表","head":[["meta",{"property":"og:url","content":"https://LincDocs.github.io/MdNote_Public/03.%20SkillAndProduction/%E8%BE%93%E5%85%A5%E6%B3%95/diy%E7%A0%81%E8%A1%A8.html"}],["meta",{"property":"og:site_name","content":"MdNote_Public"}],["meta",{"property":"og:title","content":"diy码表"}],["meta",{"property":"og:description","content":"diy码表 目录 v1 1码区 数出 基本 字词分 1码：10重 2码：1出字，2出词，4出成语 数字区 1234 56 7890 码区 qa 双拼声韵表"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2025-01-22T04:20:37.000Z"}],["meta",{"property":"article:author","content":"LincDocs"}],["meta",{"property":"article:modified_time","content":"2025-01-22T04:20:37.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"diy码表\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2025-01-22T04:20:37.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"LincDocs\\",\\"url\\":\\"https://github.com/LincDocs/MdNote_Public/\\"}]}"]]},"headers":[{"level":1,"title":"diy码表","slug":"diy码表","link":"#diy码表","children":[]},{"level":1,"title":"目录","slug":"目录","link":"#目录","children":[]},{"level":1,"title":"v1","slug":"v1","link":"#v1","children":[{"level":2,"title":"1码区","slug":"_1码区","link":"#_1码区","children":[]}]},{"level":1,"title":"数出","slug":"数出","link":"#数出","children":[{"level":2,"title":"基本","slug":"基本","link":"#基本","children":[]},{"level":2,"title":"码区","slug":"码区","link":"#码区","children":[]},{"level":2,"title":"双拼声韵表","slug":"双拼声韵表","link":"#双拼声韵表","children":[]}]}],"git":{"createdTime":1737519637000,"updatedTime":1737519637000,"contributors":[{"name":"Linc","email":"762699299@qq.com","commits":1}]},"readingTime":{"minutes":0.81,"words":244},"filePathRelative":"03. SkillAndProduction/输入法/diy码表.md","localizedDate":"2025年1月22日","excerpt":"\\n<h1>目录</h1>\\n<h1>v1</h1>\\n<h2>1码区</h2>\\n<div class=\\"language-text\\" data-ext=\\"text\\" data-title=\\"text\\"><pre class=\\"language-text\\"><code>' 1码区\\nq,1=去\\nq,2=却\\nq,3=全\\nw,1=我\\nw,2=为 ' 因为以为称为是为不为\\nwu,1=无\\nww,1=未\\ne,1=二\\ne,2=而\\nr,1=人\\nr,2=入\\nbi,1=比\\nbi,2=必\\n\\nd,1=的\\nd,2=但\\nd,3=打\\nyu,2=于是\\n' ku,2=可是\\n' du,2=但是\\n' bu,2=不是\\n' ju,2=就是\\nj,2=仅\\nvu,1=主\\nvu,2=只是\\n' 这是\\nky,2=可以\\nyw,1=因为\\nyw,2=以为\\niw,2=成为\\nuw,2=是为\\nbw,2=不为\\nny,1=那样\\nnay,1=哪样\\nnav,1=哪种\\nnag,1=哪个\\nsm,1=什么\\nwsm,1=为什么\\nusm,1=是什么\\ngsm,1=干什么\\n' 无理\\nkw,2=因为\\n\\n</code></pre></div>","autoDesc":true,"bioChainData":{"outlink":[],"backlink":[],"localMap":{"nodes":[{"id":"03. SkillAndProduction/输入法/diy码表.md","value":{"title":"diy码表","path":"03. SkillAndProduction/输入法/diy码表.md","outlink":[],"backlink":[]}}],"links":[]}}}`);export{c as comp,t as data};
